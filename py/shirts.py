27f34057fa3c425159263d872fd2d9ffee43361f
EllieH <ellie.homan@hotmail.com>
2019-06-22 10:15:18 +0100

    Renamed folders

--- py/shirts.py
+++ py/shirts.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 app = Flask("MyApp")
 
-@app.route("/")
-def hello():
-    return render_template("tshirtresultpage.html", image_path="../images/shirt1.jpeg", text_description="You're looking a little plain!")
+@app.route("/tshirtresults")
+def Load_Image_Text():
+    text_description = "You're feeling fruity in this jazzy tee!"
+    return render_template("tshirtresultpage.html", value=text_description)
+
 
 app.run(debug=False)
