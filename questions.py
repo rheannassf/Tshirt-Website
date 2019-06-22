4f91c379125e9c624981e76c59abc0b83b11ac05
zuazor <mrz@allabor.net>
2019-06-22 11:00:19 +0100

    added id to options

--- questions.py
+++ questions.py
@@ -2,4 +2,5 @@ from flask import Flask, render_template, request
 app = Flask("MyApp")
 @app.route("/answer", methods=["POST"])
 def answer():
-form_data = request.form
+form_data = request.form['purpose']
+return form_data
