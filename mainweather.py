008741a9afd5231e36b170b9656cc0d4d42a919e
Rheanna <rheanna.filmer@hotmail.com>
2019-06-25 18:10:13 +0100

    25062019_04

--- mainweather.py
+++ mainweather.py
@@ -5,21 +5,22 @@ app = Flask(__name__)
 
 @app.route("/weather", methods=["POST"])
 def weather():
-    request.form
-    return render_template("main.html")
+    api_address = "http://api.openweathermap.org/data/2.5/weather?appid=0f87c7a3d019cd7f75c37a68bcbcd073&q="
+    city = request.form['city']
+
+    url = api_address + city
+
+    json_data = requests.get(url).json()
+    formatted_data = json_data["weather"][0]["main"]
+    return render_template("index.html", weather=formatted_data)
 
 @app.route("/")
 def index():
-    return render_template
+    return render_template('index.html')
 
-api_address = "http://api.openweathermap.org/data/2.5/weather?appid=0f87c7a3d019cd7f75c37a68bcbcd073&q="
-city = raw_input("City Name :")
+app.run()
 
-url = api_address + city
 
-json_data = requests.get(url).json()
-formatted_data = json_data["weather"][0]["main"]
 
-print(formatted_data)
 
 
